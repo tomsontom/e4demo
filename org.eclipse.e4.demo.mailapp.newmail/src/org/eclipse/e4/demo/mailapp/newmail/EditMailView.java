29b532a49b4714ede05710576ed3ceccd43092a7
Tom Schindl <tom.schindl@bestsolution.at>
2010-07-25 00:41:39 +0200

    contribution

--- org.eclipse.e4.demo.mailapp.newmail/src/org/eclipse/e4/demo/mailapp/newmail/EditMailView.java
+++ org.eclipse.e4.demo.mailapp.newmail/src/org/eclipse/e4/demo/mailapp/newmail/EditMailView.java
@@ -1,8 +1,12 @@
 package org.eclipse.e4.demo.mailapp.newmail;
 
+import javax.inject.Inject;
+
+import org.eclipse.e4.demo.mailapp.mailservice.domain.IAccount;
 import org.eclipse.swt.widgets.Composite;
 
 public class EditMailView {
+	@Inject
 	public EditMailView(Composite parent) {
 	}
 }
--- org.eclipse.e4.demo.mailapp.newmail/src/org/eclipse/e4/demo/mailapp/newmail/NewMailHandler.java
+++ org.eclipse.e4.demo.mailapp.newmail/src/org/eclipse/e4/demo/mailapp/newmail/NewMailHandler.java
@@ -10,32 +10,42 @@
  ******************************************************************************/
 package org.eclipse.e4.demo.mailapp.newmail;
 
-import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.CanExecute;
-import org.eclipse.e4.core.di.annotations.Optional;
-import org.eclipse.e4.demo.mailapp.mailservice.domain.IFolderContainer;
+import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.basic.MBasicFactory;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MTrimmedWindow;
-import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Widget;
 
 @SuppressWarnings("restriction")
 public class NewMailHandler {
 	
 	@Execute
 	public void execute(
-			@Named(IServiceConstants.ACTIVE_SHELL) Shell shell, 
-			@Named(IServiceConstants.ACTIVE_SELECTION) @Optional IFolderContainer selection) {
-		System.err.println("Execute");
-//		MTrimmedWindow window = MBasicFactory.INSTANCE.createTrimmedWindow();
-//		window.setToBeRendered(true);
-//		window.setVisible(true);
-//		MPart part = MBasicFactory.INSTANCE.createPart();
-//		part.setContributionURI("platform:/plugin/org.eclipse.e4.demo.mailapp.newmail/org.eclipse.e4.demo.mailapp.newmail.EditMailView");
-//		window.getChildren().add(part);
+			final MApplication application) {
+		final MTrimmedWindow window = MBasicFactory.INSTANCE.createTrimmedWindow();
+		window.getTags().add("newMailWindow");
+		window.setHeight(500);
+		window.setWidth(600);
+		window.setToBeRendered(true);
+		window.setVisible(true);
+		MPart part = MBasicFactory.INSTANCE.createPart();
+		part.setContributionURI("platform:/plugin/org.eclipse.e4.demo.mailapp.newmail/org.eclipse.e4.demo.mailapp.newmail.EditMailView");
+		window.getChildren().add(part);
+		application.getChildren().add(window);
+		//TODO Find better solution
+		Widget s = (Widget) window.getWidget();
+		s.addDisposeListener(new DisposeListener() {
+			
+			public void widgetDisposed(DisposeEvent e) {
+				application.getChildren().remove(window);
+			}
+		});
 	}
 	
 	@CanExecute
