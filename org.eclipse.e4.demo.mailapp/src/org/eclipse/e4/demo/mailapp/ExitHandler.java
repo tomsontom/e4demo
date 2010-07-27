b37da59c0560ca365e2156a8293715842ac96255
Tom Schindl <tom.schindl@bestsolution.at>
2010-07-27 13:24:44 +0200

    added keybindings

---
+++ org.eclipse.e4.demo.mailapp/src/org/eclipse/e4/demo/mailapp/ExitHandler.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2010 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.e4.demo.mailapp;
+
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.e4.ui.workbench.IPresentationEngine;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+@SuppressWarnings("restriction")
+public class ExitHandler {
+	
+	@Execute
+	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell shell,  IPresentationEngine engine) {
+		if( MessageDialog.openQuestion(shell, "Exit Application?", "Do you really want to exit the application?") ) {
+			engine.stop();	
+		}
+	}
+}
