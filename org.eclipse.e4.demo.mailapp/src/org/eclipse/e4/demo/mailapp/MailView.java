25dc86ade8febf8c977dc9b353c7fa6ae74fdb4a
Tom Schindl <tom.schindl@bestsolution.at>
2010-07-27 21:27:58 +0200

    EPL + EDL

--- org.eclipse.e4.demo.mailapp/src/org/eclipse/e4/demo/mailapp/MailView.java
+++ org.eclipse.e4.demo.mailapp/src/org/eclipse/e4/demo/mailapp/MailView.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2010 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of:
+ * 
+ * a) EPL
+ * The Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * b) EDL
+ * The Eclipse Development License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.e4.demo.mailapp;
 
 import org.eclipse.core.databinding.DataBindingContext;
